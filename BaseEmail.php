015187810e7d0b0da88ae1558ba000c7ddba7056
azole <azolelai@gmail.com>
2014-03-11 15:43:35 +0800

    add defaultTitle and refactor getTitle

--- BaseEmail.php
+++ BaseEmail.php
@@ -10,22 +10,47 @@ abstract class BaseEmail extends Email
 
     public $templateHandle;
 
+    public $defaultTitle;
+
+    public function getTitle() {
+
+        if ( $this->useModelTemplate ) {    // from db
+
+            if ( $this->templateHandle ) {
+                $mailTemplate = new EmailTemplate([ 'handle' => $this->templateHandle ]);                
+                return empty($mailTemplate->subject) ? $this->defaultTitle : $mailTemplate->subject;
+            } else {
+                throw new Exception("Template handle is empty.");
+            }
+        } else {
+            return $this->defaultTitle;
+        }
+    }
+
     public function getContent()
     {
         if ( $this->useModelTemplate ) {    // from db
 
             if ( $this->templateHandle ) {
                 $mailTemplate = new EmailTemplate([ 'handle' => $this->templateHandle ]);
-                // reset subject from db
-                $this['subject'] = kernel()->getApplicationName() . ' - ' . $mailTemplate->subject;
-                $this->setSubject( $this['subject'] );
             } else {
                 throw new Exception("Template handle is empty.");
             }
 
-            $loader = new \Twig_Loader_String();
-            $twig = new \Twig_Environment($loader);
-            return $twig->render($mailTemplate->content, $this->getData());
+            $fsLoader = kernel()->twig->loader;   // framework 提供的 file system loader
+
+            $arrayLoader = new \Twig_Loader_Array(array(
+                'email.html' => $mailTemplate->content
+            ));
+            
+            // 把 arrayLoader 跟 fsLoader 丟給 chainLoader
+            $chainLoader = new \Twig_Loader_Chain(array($fsLoader, $arrayLoader));
+
+            // 另外 create 一個新的 twig environment
+            $twig = new \Twig_Environment($chainLoader);
+
+            return $twig->render('email.html',  $this->getData());
+
         } else {    // from templates
             return parent::getContent();
         }
