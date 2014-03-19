c1f1c32ae083dcb1082299ae565b4b7a0e73cc6e
c9s <yoanlin93@gmail.com>
2014-03-20 00:06:26 +0800

    use getTemplateSubPath to get the correct email template path.

--- BaseEmail.php
+++ BaseEmail.php
@@ -3,22 +3,44 @@ namespace EmailBundle;
 use Phifty\Message\Email;
 use EmailBundle\Model\EmailTemplate;
 use Exception;
+use ReflectionObject;
+use Twig_Loader_Array;
+use Twig_Loader_Chain;
+use Twig_Environment;
+use Twig_Loader_String;
 
 abstract class BaseEmail extends Email
 {
+
+    /**
+     * @var boolean Should we use the email template from database ?
+     */
     public $useModelTemplate = false;  // 預設是關閉的
 
+    /**
+     * @var string $templateHandle is used for finding template files or a email 
+     *             template entry in database.
+     */
     public $templateHandle;
 
+
+    /**
+     * @var string pre-defined email title
+     *
+     * A title is a part of a subject, that is, subject includes site name as its prefix 
+     * and append the title.
+     */
     public $defaultTitle;
 
+    /**
+     * @var string default language, optional.
+     */
     public $lang;
 
     public function getTitle() {
-
         if ( $this->useModelTemplate ) {    // from db
-
             if ( $this->templateHandle ) {
+                // get the current locale as it's handle.
                 if(!$this->lang) {
                     $this->lang = kernel()->locale->current();
                 }
@@ -29,16 +51,15 @@ abstract class BaseEmail extends Email
                 if ( empty($mailTemplate->title)) {
                     return $this->defaultTitle;
                 } else {
-                    $loader = new \Twig_Loader_String();
-                    $twig = new \Twig_Environment($loader);
+                    $loader = new Twig_Loader_String();
+                    $twig = new Twig_Environment($loader);
                     return $twig->render($mailTemplate->title, $this->getData());
                 }
             } else {
                 throw new Exception("Template handle is empty.");
             }
-        } else {
-            return $this->defaultTitle;
         }
+        return $this->defaultTitle;
     }
 
     public function getContent()
@@ -59,12 +80,13 @@ abstract class BaseEmail extends Email
 
             $fsLoader = kernel()->twig->loader;   // framework 提供的 file system loader
 
-            $arrayLoader = new \Twig_Loader_Array(array(
+            // a custom array loader with email.html template
+            $arrayLoader = new Twig_Loader_Array(array(
                 'email.html' => $mailTemplate->content
             ));
             
             // 把 arrayLoader 跟 fsLoader 丟給 chainLoader
-            $chainLoader = new \Twig_Loader_Chain(array($fsLoader, $arrayLoader));
+            $chainLoader = new Twig_Loader_Chain(array($fsLoader, $arrayLoader));
 
             // 另外 create 一個新的 twig environment
             $twig = new \Twig_Environment($chainLoader);
@@ -74,6 +96,20 @@ abstract class BaseEmail extends Email
         } else {    // from templates
             return parent::getContent();
         }
+    }
+
+    /**
+     * This method returns a template file path in this format:
+     *
+     * @return string template file path for twig
+     *
+     *      @{bundleName}/email/{lang}/{templateHandle}.html
+     */
+    public function getTemplateSubPath() {
+        $ro = new ReflectionObject($this);
+        $args = explode('\\',$ro->getNamespaceName());
+        $topNs = $args[0]; // top level namespace
+        return join('/','email', '@' . $topNs, $lang, $this->templateHandle . '.html');
     }
 }
 
--- Model/EmailTemplate.php
+++ Model/EmailTemplate.php
@@ -3,8 +3,15 @@ namespace EmailBundle\Model;
 
 class EmailTemplate  extends \EmailBundle\Model\EmailTemplateBase {
 
-
-
+    /**
+     * @retunr EmailTemplate
+     */
+    public static function loadByHandle($handle, $lang = null) {
+        $lang = $lang ?: kernel()->locale->current();
+        $record = new self;
+        $record->load([ 'handle' => $handle, 'lang' => $lang ]);
+        return $record;
+    }
 
     /**
      * Code block for message id parser.
